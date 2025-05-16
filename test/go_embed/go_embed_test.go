1a6c32cc24f809425111e7cd35a583dd4e147928
toquery <toquery@qq.com>
2025-05-16 20:32:23 +0800

    init

--- test/go_embed/go_embed_test.go
+++ test/go_embed/go_embed_test.go
@@ -26,14 +26,37 @@ func TestGEByte(t *testing.T) {
 }
 
 //go:embed assets
-var fs embed.FS
+var gea embed.FS
+
+//go:embed all:assets
+var geaa embed.FS
+
+//go:embed assets/*
+var gea_ embed.FS // 只包含 assets/ 下的文件，不递归
+
+//go:embed all:assets/*
+var geaa_ embed.FS // 包含整个 assets/ 目录及子目录、文件
 
 func TestGEReadDir(t *testing.T) {
-	dirEntries, _ := fs.ReadDir("assets")
+	fmt.Printf("\n\n[go:embed assets]\n")
+	PrintDirAndSubDir(gea)
+
+	fmt.Printf("\n\n[go:embed all:assets]\n")
+	PrintDirAndSubDir(geaa)
+
+	fmt.Printf("\n\n[go:embed assets/*]\n")
+	PrintDirAndSubDir(gea_)
+
+	fmt.Printf("\n\n[go:embed all:assets/*]\n")
+	PrintDirAndSubDir(geaa_)
+}
+
+func PrintDirAndSubDir(assets embed.FS) {
+	dirEntries, _ := assets.ReadDir("assets")
 	for _, de := range dirEntries {
 		fmt.Printf("assets/%s IsDir=%t \n", de.Name(), de.IsDir())
 	}
-	publicDirEntries, _ := fs.ReadDir("assets/public")
+	publicDirEntries, _ := assets.ReadDir("assets/public")
 	for _, de := range publicDirEntries {
 		fmt.Printf("assets/public/%s IsDir=%t \n", de.Name(), de.IsDir())
 	}
